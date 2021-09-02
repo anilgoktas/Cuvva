3f555f4028045940c48dd3d0f623efc6566e252a
Anil Goktas <goktas.anil@gmail.com>
2021-09-02 12:41:51 +0300

    Implement JSONEvent decoding and unit tests

--- CuvvaTechTest/API/JSONModel.swift
+++ CuvvaTechTest/API/JSONModel.swift
@@ -16,9 +16,38 @@ let apiJsonDecoder: JSONDecoder = {
 
 typealias JSONResponse = [JSONEvent]
 
-struct JSONEvent: Decodable, Identifiable {
-
-    let id: String
-
-    #warning("// TODO: Add remaining properties")
+struct JSONEvent: Decodable {
+    
+    enum Event: String, Decodable, CaseIterable {
+        case policy_created
+        case policy_extension
+        case policy_cancelled
+    }
+    
+    struct Payload: Decodable {
+        
+        struct Vehicle: Decodable {
+            let prettyVrm: String
+            let make: String
+            let model: String
+            
+            enum CodingKeys: String, CodingKey {
+                case prettyVrm = "prettyVrm"
+                case make
+                case model
+            }
+        }
+        
+        let timestamp: Date
+        let policyId: String
+        let originalPolicyId: String?
+        let startDate: Date?
+        let endDate: Date?
+        let vehicle: Vehicle?
+        /// May exist when type is `policy_cancelled`
+        let newEndDate: Date?
+    }
+    
+    let type: Event
+    let payload: Payload
 }
--- CuvvaTechTestTests/JSONModelTests.swift
+++ CuvvaTechTestTests/JSONModelTests.swift
@@ -38,8 +38,8 @@ extension JSONModelTests {
         
         let jsonString = """
         {
-        "id": "\(id)",
-        "snake_case_string": "\(snakeCaseString)"
+            "id": "\(id)",
+            "snake_case_string": "\(snakeCaseString)"
         }
         """
         let jsonData = Data(jsonString.utf8)
@@ -63,8 +63,8 @@ extension JSONModelTests {
         
         let jsonString = """
         {
-        "id": "\(id)",
-        "date": "\(dateString)",
+            "id": "\(id)",
+            "date": "\(dateString)",
         }
         """
         let jsonData = Data(jsonString.utf8)
@@ -80,6 +80,117 @@ extension JSONModelTests {
         let expectedDate = try XCTUnwrap(dateFormatter.date(from: dateString))
         XCTAssertEqual(testStruct.date.timeIntervalSince1970, expectedDate.timeIntervalSince1970)
         XCTAssertEqual(dateFormatter.string(from: testStruct.date), dateString)
+    }
+    
+}
+
+// MARK: - JSONEvent Tests
+
+extension JSONModelTests {
+    
+    /// Based on API response structure.
+    private func makeJSONEventData(
+        type: JSONEvent.Event = .policy_created,
+        timestampString: String = "2021-08-31T18:11:31.031Z",
+        policyId: String = "dev_pol_0000003",
+        originalPolicyId: String? = nil,
+        startDateString: String? = nil,
+        endDateString: String? = nil,
+        vehicle: JSONEvent.Payload.Vehicle? = nil,
+        newEndDateString: String? = nil
+    ) throws -> Data {
+        var dict = [String: Any]()
+        dict["type"] = type.rawValue
+        
+        var payloadDict = [String: Any]()
+        payloadDict["timestamp"] = timestampString
+        payloadDict["policy_id"] = policyId
+        payloadDict["original_policy_id"] = originalPolicyId
+        payloadDict["start_date"] = startDateString
+        payloadDict["end_date"] = endDateString
+        payloadDict["new_end_date"] = newEndDateString
+        
+        if let vehicle = vehicle {
+            var vehicleDict = [String: Any]()
+            vehicleDict["prettyVrm"] = vehicle.prettyVrm
+            vehicleDict["make"] = vehicle.make
+            vehicleDict["model"] = vehicle.model
+            payloadDict["vehicle"] = vehicleDict
+        }
+        dict["payload"] = payloadDict
+        
+        return try JSONSerialization.data(withJSONObject: dict, options: [])
+    }
+    
+    func test_jsonEvent_decoding_events() throws {
+        for policyType in JSONEvent.Event.allCases {
+            try autoreleasepool {
+                // Given
+                let jsonData = try makeJSONEventData(type: policyType)
+                
+                // When
+                let jsonEvent = try apiJsonDecoder.decode(JSONEvent.self, from: jsonData)
+                
+                // Then
+                XCTAssertEqual(jsonEvent.type, policyType)
+            }
+        }
+    }
+    
+    func test_jsonEvent_decoding_payloadWithoutVehicle() throws {
+        // Given
+        let timestampString = "2021-08-31T18:11:31.031Z"
+        let policyId = "dev_pol_0000003"
+        let originalPolicyId = "dev_pol_0000002"
+        let startDateString = "2021-08-31T18:11:31.031Z"
+        let endDateString = "2021-08-31T19:11:31.031Z"
+        let jsonData = try makeJSONEventData(
+            timestampString: timestampString,
+            policyId: policyId,
+            originalPolicyId: originalPolicyId,
+            startDateString: startDateString,
+            endDateString: endDateString
+        )
+        
+        // When
+        let jsonEvent = try apiJsonDecoder.decode(JSONEvent.self, from: jsonData)
+        
+        // Then
+        XCTAssertEqual(jsonEvent.payload.policyId, policyId)
+        XCTAssertEqual(jsonEvent.payload.originalPolicyId, originalPolicyId)
+        
+        let dateFormatter = try XCTUnwrap(dateFormatter)
+        let timestamp = try XCTUnwrap(jsonEvent.payload.timestamp)
+        XCTAssertEqual(dateFormatter.string(from: timestamp), timestampString)
+        let startDate = try XCTUnwrap(jsonEvent.payload.startDate)
+        XCTAssertEqual(dateFormatter.string(from: startDate), startDateString)
+        let endDate = try XCTUnwrap(jsonEvent.payload.endDate)
+        XCTAssertEqual(dateFormatter.string(from: endDate), endDateString)
+        
+        XCTAssertNil(jsonEvent.payload.vehicle)
+        XCTAssertNil(jsonEvent.payload.newEndDate)
+    }
+    
+    func test_jsonEvent_decoding_payloadVehicle() throws {
+        // Given
+        let prettyVRM = "MA77 GRO"
+        let maker = "Volkswagen"
+        let model = "Polo"
+        let vehicle = JSONEvent.Payload.Vehicle(
+            prettyVrm: prettyVRM,
+            make: maker,
+            model: model
+        )
+        let jsonData = try makeJSONEventData(vehicle: vehicle)
+        
+        // When
+        let jsonEvent = try apiJsonDecoder.decode(JSONEvent.self, from: jsonData)
+        
+        // Then
+        let payloadVehicle = try XCTUnwrap(jsonEvent.payload.vehicle)
+        XCTAssertEqual(payloadVehicle.prettyVrm, prettyVRM)
+        XCTAssertEqual(payloadVehicle.make, maker)
+        XCTAssertEqual(payloadVehicle.model, model)
     }
     
 }
