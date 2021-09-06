0b1b30e18aba51269d58f4ed6747eb77401a976c
Anil Goktas <goktas.anil@gmail.com>
2021-09-06 11:23:04 +0300

    Policy update warning for the retain cycle

--- CuvvaTechTest/Models/Policy.swift
+++ CuvvaTechTest/Models/Policy.swift
@@ -4,7 +4,7 @@ final class Policy: ObservableObject, Identifiable {
     
     let id: String
     let term: PolicyTerm
-    #warning("Retain cycle, weak?")
+    #warning("Retain cycle, weak? All vehicles already cached in `LivePolicyEventProcessor` with a reference")
     let vehicle: Vehicle
     
     init(id: String, term: PolicyTerm, vehicle: Vehicle) {
